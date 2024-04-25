81d59f2388b6f858d8420c9149af4cd81130afd1
Julio Villalvazo <julio.villalvazo@encora.com>
2024-04-24 20:27:59 -0600

    test: added testing to create an expresion impresa request

--- src/test/java/com/encora/easysat/EasysatApplicationTests.java
+++ src/test/java/com/encora/easysat/EasysatApplicationTests.java
@@ -1,6 +1,9 @@
 package com.encora.easysat;
 
+import com.example.consumingwebservice.wsdl.ObjectFactory;
+import jakarta.xml.bind.JAXBElement;
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
@@ -8,6 +11,17 @@ class EasysatApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void testCreateConsultaExpresionImpresa() {
+		ObjectFactory factory = new ObjectFactory();
+		String expectedSoapRequest = "expectedSoapRequest";
+		JAXBElement<String> result = factory.createConsultaExpresionImpresa(expectedSoapRequest);
+
+		assertNotNull(result);
+		assertEquals(expectedSoapRequest, result.getValue());
+
 	}
 
 }
